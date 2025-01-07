08b6494c8fa238febb08fcda366131603e6bb2ed
sava8sava <supremomelao@gmail.com>
2025-01-07 20:53:03 -0300

    adicionado classe escala de cinza

--- AV#/filtros.py
+++ AV#/filtros.py
@@ -7,7 +7,7 @@ from Imagem import Imagem
 class B_and_W_filter:
     
     @staticmethod
-    def apply_filter(img : Imagem):
+    def apply_filter(img : Imagem) -> Imagem:
         aux = img
         if img.get_img():
             filtered_img = aux.get_img()
@@ -23,6 +23,23 @@ class B_and_W_filter:
 #filtro cartoon
 
 #escala de cinza
+class Gray_scale_filter:
+
+    @staticmethod
+    def apply_filter(img : Imagem) -> Imagem:
+        aux = img
+        if img.get_img():
+            #fazer uma pequena alteração para que a pessoa posso colocar o nome do arquivo
+            filtered_img = aux.get_img()
+
+            filtered_img = filtered_img.convert('L')
+
+            aux.set_img(filtered_img)
+
+            filtered_img.save("AV#/imagens teste/result(cinza).jpg",)
+
+            return aux
+
 
 #filtro negativo 
 
--- AV#/teste.py
+++ AV#/teste.py
@@ -3,5 +3,5 @@ from filtros import *
 
 imagem = Imagem()
 imagem.set_public_img(r"https://www.revistaveterinaria.com.br/wp-content/uploads/2018/11/gatinho-preguicoso1.jpg")
-aux = B_and_W_filter.apply_filter(imagem)
+aux = Gray_scale_filter.apply_filter(imagem)
 aux.show_image()
