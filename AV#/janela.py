7a56897b1dfa9c5b16d42dc96d09f6ce4136cb70
LuandersonTorres <luanderson.torres@ufpi.edu.br>
2025-01-19 01:57:51 -0300

    Update janela.py

--- AV#/janela.py
+++ AV#/janela.py
@@ -2,38 +2,36 @@ from tkinter import *
 from Imagem import Imagem
 from filtros import *
 from functools import *
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from tkinter import ttk
 
 imagem = Imagem()
 
 def bt1_click():
     arquivos = filedialog.askopenfilenames()
+    if not arquivos:
+        lb1["text"] = "Nenhum arquivo selecionado."
+        messagebox.showwarning("Erro", "Por favor, adicione uma imagem ou URL!")
+        return  
+    
     imagem.set_local_img(arquivos[0])
-    if arquivos:
-        imagem.set_local_img(arquivos[0])
-        lb1["text"]=f"Imagem carregada:{arquivos[0]}"
-    else:
-       lb1["text"]="Nenhum arquivo selecionado."
+    lb1["text"] = f"Imagem carregada: {arquivos[0]}"
 
 def bt2_click():
+
     filtro=cb.get()
     if filtro=="Preto e Branco":
         uxbw = B_and_W_filter.apply_filter(imagem)
         uxbw.show_image()
 
-        lb1["text"]="exibido"
-
     if filtro=="Escala Cinza":
         auxgray = Gray_scale_filter.apply_filter(imagem)
         auxgray.show_image()
-        lb1["text"]="exibido"
 
     if filtro=="Efeito Blurr":
         auxblurr = Blurr_filter.apply_filter(imagem)
         auxblurr.show_image()
 
-    
     if filtro=="Filtro Negativo":
         auxnegative = Negative_filter.apply_filter(imagem)
         auxnegative.show_image()
@@ -42,48 +40,66 @@ def bt2_click():
         auxcontorno = Contour_filter.apply_filter(imagem)
         auxcontorno.show_image()
 
+    messagebox.showinfo("Imagem carregada!","A imagem foi exibida com o filtro escolhido.")
+
 def bt3_click():
     link = ed.get()
     if link:
         imagem.set_public_img(link)
-        lb1["text"]=f"Imagem carregada"
+        messagebox.showinfo("Imagem carregada!","A imagem ecolhida foi carregada com sucesso!")
+
+def bt4_click():
+    exit()
 
 
 janela = Tk()
 
-janela.title("trabalho final")
+janela.title("Trabalho Final")
 
-lb = Label(janela, text="selecione: ")
-lb.place(x=100, y=100)
+lb1=Label(janela,text="BEM-VINDO!")
+lb1.pack()
 
-lb1=Label(janela,text="bem vindo")
-lb1.place(x=100,y=220)
+lb = Label(janela, text="")
+lb.pack()
 
-bt1 =Button(janela, width=10, text="img local")
-bt1.place(x=180,y=100)
+lb = Label(janela, text="Selecione: ")
+lb.pack()
+
+bt1 =Button(janela, width=10, text="Imagem local")
+bt1.pack()
 bt1["command"] = partial(bt1_click)
 
-lb3 = Label(janela, text="internet:")
-lb.place(x=280, y=100)
+lb = Label(janela, text="ou")
+lb.pack()
+
+lb = Label(janela, text="Internet:")
+lb.pack()
 
 ed= Entry(janela)
-ed.place(x=340, y=100)
+ed.pack()
 
-bt3 =Button(janela, width=8, text="baixar",command=bt3_click)
-bt3.place(x=480, y=95)
+bt3 =Button(janela, width=8, text="Baixar",command=bt3_click)
+bt3.pack()
 
-bt2 =Button(janela, width=8, text="aplicar",command=bt2_click)
-bt2.place(x=250,y=150)
+lb = Label(janela, text="")
+lb.pack()
 
-lb2=Label(janela,text="filtros")
-lb2.place(x=100,y=130)
+lb2=Label(janela,text="Filtros:")
+lb2.pack()
 
 listFiltros=["Preto e Branco","Escala Cinza","Efeito Blurr","Filtro Negativo","Filtro Contorno"]
 
 cb=ttk.Combobox(janela,values=listFiltros)
-cb.place(x=180,y=130)
+cb.pack()
 
+bt2 =Button(janela, width=8, text="Aplicar",command=bt2_click)
+bt2.pack()
 
+lb = Label(janela, text="")
+lb.pack()
+
+bt4 =Button(janela, width=8, text="Sair",command=bt4_click)
+bt4.pack()
 
 janela.geometry("600x600+200+200")
 
